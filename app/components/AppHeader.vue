a9c5451586a6aad7d1a3276618bf7ed8ad9e29ee
Serhii <tustanovskiis@gmail.com>
2026-02-16 15:51:14 +0100

    feat(AppHeader.vue): added  component

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="app-header">
+    <div class="container">
+      <div class="app-header__wrapper">
+        header
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.app-header {
+  padding: rem(16);
+
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+@include media-query('tablet') {
+  .app-header {
+    padding: rem(32);
+  }
+}
+</style>
