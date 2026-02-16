6ee9bcd1c12136067f94c3ec7a7b1c45f7a34675
Serhii <tustanovskiis@gmail.com>
2026-02-16 16:33:06 +0100

    feat: added dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/style.scss" as *;',
+        },
+      },
+    },
+  },
+  components: [
+    { path: '~/components', pathPrefix: false },
+  ],
 })
