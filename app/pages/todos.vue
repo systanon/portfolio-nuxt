edda689e239447ef86b4ad8f015045a68fa01baa
Serhii <tustanovskiis@gmail.com>
2026-02-19 10:30:49 +0100

    feat: create todos components

---
+++ app/pages/todos.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="page-todo">
+    <h2 class="page-todo__title">This is Todos page</h2>
+    <UiButtonIcon
+      class="page-todo__create"
+      iconName="plus"
+      iconColor="tertiary"
+    >
+      <template #prepend>
+        <span class="page-todo__create-text"> Create todo </span>
+      </template>
+    </UiButtonIcon>
+    <section class="page-todo__todos">
+      <TodoItem v-for="todo of rows" :key="todo.id" :todo="todo" />
+    </section>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { useTodoStore } from '~/store/todo'
+import type { Todo } from '~/types/todo'
+const todoStore = useTodoStore()
+const { pages, rows } = storeToRefs(todoStore)
+const { getAll, update, create, remove } = todoStore
+
+onMounted(() => {
+  getAll()
+  todoStore.initWS()
+})
+
+onUnmounted(() => {
+  todoStore.destroyWS()
+})
+</script>
