cecc3de642920f4178b56d4b4206cca232f42339
Serhii <tustanovskiis@gmail.com>
2026-02-23 12:26:53 +0100

    fix(http.client): handle non-2xx responses correctly

--- app/lib/http.client.ts
+++ app/lib/http.client.ts
@@ -17,27 +17,38 @@ export class HTTPClient {
   ): Promise<AppSuccess<T> | AppError | AppRateLimitError | AppSilentError> {
     try {
       const response = await this.fetcher.raw(url, options)
-      if (!response.ok) {
-        const data = response._data as ErrorResponse
+      const data = response._data as SuccessResponse<T>
+      return new AppSuccess<T>(data.data, response.headers, data.message)
+    } catch (error: unknown) {
+      if (this.isFetchError(error)) {
+        const data = error.response._data as ErrorResponse
         if (data.error.code === 'RATE_LIMIT') {
           return new AppRateLimitError(
             data.error.message,
-            Number(response.headers.get('Retry-After')),
+            Number(error.response.headers.get('Retry-After')),
           )
         } else if (data.error.code === 'UNAUTHORIZED') {
           return new AppSilentError(data.error.message, { cause: data.error })
         } else {
-          return new AppError(data.error.message)
+          return new AppError(data.error.message ?? 'Unknown error', {
+            cause: data.error,
+          })
         }
-      } else {
-        const data = response._data as SuccessResponse<T>
-        return new AppSuccess<T>(data.data, response.headers, data.message)
       }
-    } catch (error: unknown) {
       if (error instanceof Error) {
         return new AppError(error.message, { cause: error })
       }
       return new AppError('Unknown error', { cause: error })
     }
+  }
+
+  private isFetchError(error: any): error is {
+    response: {
+      _data?: ErrorResponse
+      status: number
+      headers: Headers
+    }
+  } {
+    return error && error.response
   }
 }
