dae3cc6254834fb28d6397f9719a77c14abc2286
Serhii <tustanovskiis@gmail.com>
2026-02-23 16:51:52 +0100

    feat(http.client): used interceptors

--- app/lib/http.client.ts
+++ app/lib/http.client.ts
@@ -1,53 +1,108 @@
 import type { $Fetch, NitroFetchOptions } from 'nitropack'
 import type { ErrorResponse, SuccessResponse } from '~/types/api'
 import { AppError, AppRateLimitError, AppSilentError } from '~/types/app-errors'
-
 import { AppSuccess } from '~/types/app.types'
+
+type RequestInterceptor = (
+  url: string,
+  options: NitroFetchOptions<'json'>,
+) => Promise<void> | void
+
+export interface RetryableOptions extends NitroFetchOptions<'json'> {
+  _retry?: boolean
+}
+
+type ResponseInterceptor = (response: Response) => Promise<void> | void
+
+type ErrorInterceptor = (
+  error: any,
+  retry: () => Promise<any>,
+  options: NitroFetchOptions<'json'>,
+) => Promise<boolean | void> | boolean | void
 
 export class HTTPClient {
   private fetcher: $Fetch
 
-  constructor(fetch: $Fetch) {
-    this.fetcher = fetch
+  private requestInterceptors: RequestInterceptor[] = []
+  private responseInterceptors: ResponseInterceptor[] = []
+  private errorInterceptors: ErrorInterceptor[] = []
+
+  constructor(fetcher: $Fetch) {
+    this.fetcher = fetcher
   }
 
-  public async do<T>(
+  addRequestInterceptor(fn: RequestInterceptor) {
+    this.requestInterceptors.push(fn)
+  }
+
+  addResponseInterceptor(fn: ResponseInterceptor) {
+    this.responseInterceptors.push(fn)
+  }
+
+  addErrorInterceptor(fn: ErrorInterceptor) {
+    this.errorInterceptors.push(fn)
+  }
+
+  async do<T>(
     url: string,
     options: NitroFetchOptions<'json'> = {},
   ): Promise<AppSuccess<T> | AppError | AppRateLimitError | AppSilentError> {
-    try {
-      const response = await this.fetcher.raw(url, options)
-      const data = response._data as SuccessResponse<T>
-      return new AppSuccess<T>(data.data, response.headers, data.message)
-    } catch (error: unknown) {
-      if (this.isFetchError(error)) {
-        const data = error.response._data as ErrorResponse
-        if (data.error.code === 'RATE_LIMIT') {
-          return new AppRateLimitError(
-            data.error.message,
-            Number(error.response.headers.get('Retry-After')),
-          )
-        } else if (data.error.code === 'UNAUTHORIZED') {
-          return new AppSilentError(data.error.message, { cause: data.error })
-        } else {
-          return new AppError(data.error.message ?? 'Unknown error', {
-            cause: data.error,
-          })
+    const exec = async () => {
+      try {
+        for (const i of this.requestInterceptors) {
+          await i(url, options)
         }
+
+        const response = await this.fetcher.raw(url, options)
+
+        for (const i of this.responseInterceptors) {
+          await i(response)
+        }
+
+        const data = response._data as SuccessResponse<T>
+        return new AppSuccess<T>(data.data, response.headers, data.message)
+      } catch (error) {
+        for (const i of this.errorInterceptors) {
+          const shouldRetry = await i(error, exec, options)
+          if (shouldRetry) return exec()
+        }
+
+        return this.handleError(error)
       }
-      if (error instanceof Error) {
-        return new AppError(error.message, { cause: error })
-      }
-      return new AppError('Unknown error', { cause: error })
     }
+
+    return exec()
+  }
+
+  private handleError(error: any) {
+    if (this.isFetchError(error)) {
+      const data = error.response._data as ErrorResponse
+
+      if (data.error.code === 'RATE_LIMIT') {
+        return new AppRateLimitError(
+          data.error.message,
+          Number(error.response.headers.get('Retry-After')),
+        )
+      }
+
+      if (data.error.code === 'UNAUTHORIZED') {
+        return new AppSilentError(data.error.message, { cause: data.error })
+      }
+
+      return new AppError(data.error.message ?? 'Unknown error', {
+        cause: data.error,
+      })
+    }
+
+    if (error instanceof Error) {
+      return new AppError(error.message, { cause: error })
+    }
+
+    return new AppError('Unknown error', { cause: error })
   }
 
   private isFetchError(error: any): error is {
-    response: {
-      _data?: ErrorResponse
-      status: number
-      headers: Headers
-    }
+    response: { _data?: ErrorResponse; status: number; headers: Headers }
   } {
     return error && error.response
   }
