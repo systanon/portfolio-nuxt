a81a2061b115cc34240656d869a42b349e79c85b
Serhii <tustanovskiis@gmail.com>
2026-03-04 13:14:57 +0100

    feat(hideSplash): implement hide splash animation

---
+++ app/utils/animations/hideSplash.ts
@@ -0,0 +1,39 @@
+import type { gsap as GSAPType } from 'gsap'
+export function hideSplash(gsap: typeof GSAPType) {
+  return () => {
+    let hideTl: gsap.core.Timeline | null = null
+
+    const hideSplash = (
+      container: HTMLElement | null,
+      cb?: any,
+    ): Promise<void> => {
+      if (!container) return Promise.resolve()
+      return new Promise((resolve) => {
+        if (hideTl) hideTl.kill()
+
+        hideTl = gsap.timeline({
+          onComplete: () => {
+            cb?.()
+            resolve()
+          },
+        })
+
+        hideTl.to(container, {
+          opacity: 0,
+          scale: 1.015,
+          duration: 1.0,
+          ease: 'power3.inOut',
+        })
+      })
+    }
+
+    const kill = (): void => {
+      hideTl?.kill()
+    }
+
+    return {
+      hideSplash,
+      kill,
+    }
+  }
+}
