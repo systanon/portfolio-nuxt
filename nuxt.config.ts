53798a3cbc269add942fa92437e6ee6d7d3104a5
Serhii <tustanovskiis@gmail.com>
2026-03-27 21:47:40 +0100

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    },
+  },
   modules: ['nuxt-svg-sprite-icon', '@pinia/nuxt', '@vueuse/nuxt'],
   svgSprite: {
     input: '~/assets/icons',
