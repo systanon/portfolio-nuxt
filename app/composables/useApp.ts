8b06feb88d18eb5186fb77339046faea71a20d6f
Serhii <tustanovskiis@gmail.com>
2026-04-17 13:40:10 +0200

    fix: app typing

--- app/composables/useApp.ts
+++ app/composables/useApp.ts
@@ -1,4 +1,14 @@
+import { ClientApplication } from '~/application/clientApplication'
+
 export const useApp = () => {
-  const { $appInstance } = useNuxtApp()
-  return $appInstance
+  const { $application } = useNuxtApp()
+  return $application
+}
+
+export const useClientApp = () => {
+  const { $application } = useNuxtApp()
+  if (!($application instanceof ClientApplication)) {
+    throw new Error('useClientApp called on server')
+  }
+  return $application as ClientApplication
 }
--- app/types/nuxt.d.ts
+++ app/types/nuxt.d.ts
@@ -1,17 +1,10 @@
-import type { Application } from '~/application/application'
-import type { NotificationService } from '~/application/services/notification.service'
-
 declare module '#app' {
   interface NuxtApp {
-    $appInstance: Application
-    $notification: NotificationService
-  }
-}
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $appInstance: Application
-    $notification: NotificationService
+    $application: ClientApplication | ServerApplication
+    $httpClient: HTTPClient
+    $wsService?: WSService
+    $notification?: NotificationService
+    $syncModule?: SyncModule
   }
 }
 
