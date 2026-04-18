78d822b8c180eb0fa134166b69806cb200170d55
Serhii <tustanovskiis@gmail.com>
2026-04-18 21:27:10 +0200

    feat: devide event bus

---
+++ app/utils/event-bus.ts
@@ -0,0 +1,42 @@
+import EventEmitter from 'eventemitter3'
+import type { Profile } from '~/types/user.types'
+export interface AppEvents {
+  'auth:login': [profile: Profile]
+  'auth:logout': []
+  'app:error': [message: string]
+  'app:success': [message: string]
+  'data:loading': [isLoading: boolean]
+}
+
+export class EventBus<
+  EventTypes extends EventEmitter.ValidEventTypes = string | symbol,
+  EventContext extends any = any,
+> {
+  private ee: EventEmitter = new EventEmitter()
+
+  public on<T extends EventEmitter.EventNames<EventTypes>>(
+    event: T,
+    fn: EventEmitter.EventListener<EventTypes, T>,
+    context?: EventContext,
+  ): EventEmitter {
+    return this.ee.on(event, fn, context)
+  }
+
+  public off<T extends EventEmitter.EventNames<EventTypes>>(
+    event: T,
+    fn?: EventEmitter.EventListener<EventTypes, T>,
+    context?: EventContext,
+    once?: boolean,
+  ): EventEmitter {
+    return this.ee.off(event, fn, context, once)
+  }
+
+  public emit<T extends EventEmitter.EventNames<EventTypes>>(
+    event: T,
+    ...args: EventEmitter.EventArgs<EventTypes, T>
+  ): boolean {
+    return this.ee.emit(event, ...args)
+  }
+}
+
+export const eventBus = new EventBus<AppEvents>()
