03df5263fdb8bc62f3df071521157142e99126c1
Serhii <tustanovskiis@gmail.com>
2026-04-18 21:25:31 +0200

    feat: plugin types

--- app/types/nuxt.d.ts
+++ app/types/nuxt.d.ts
@@ -1,17 +1,31 @@
 import type { Application } from '~/application/application'
-import type { NotificationService } from '~/application/services/notification.service'
+import type { NotificationModule } from '~/application/modules/notification/notification.module'
+import type { HTTPClient } from '~/lib/http.client'
+import type { AuthService } from '~/application/services/auth.service'
+import type { UserService } from '~/application/services/user.service'
+import type { TodoService } from '~/application/services/todo.service'
+import type { NotesService } from '~/application/services/note.service'
+import type { WSService } from '~/application/services/ws.service'
 
 declare module '#app' {
   interface NuxtApp {
-    $appInstance: Application
-    $notification: NotificationService
+    $application: Application
+    $notification: NotificationModule
+    $httpClient: HTTPClient
+    $api: {
+      auth: AuthService
+      user: UserService
+      todo: TodoService
+      notes: NotesService
+      ws: WSService
+    }
   }
 }
 
 declare module 'vue' {
   interface ComponentCustomProperties {
     $appInstance: Application
-    $notification: NotificationService
+    $notification: NotificationModule
   }
 }
 
