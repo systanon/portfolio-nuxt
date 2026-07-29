59463514c273cfb529ee6fa7e66cf17cf689218a
Serhii <tustanovskiis@gmail.com>
2026-07-29 10:53:40 +0200

    feat: created application layers

---
+++ app/application/notes.application.ts
@@ -0,0 +1,54 @@
+import { AppError } from '~/types/app-errors'
+import { AppSuccess, type GetAllParams, type PaginateResult } from '~/types/app.types'
+import type { CreateNoteDTO, Note, UpdateNoteDTO } from '~/types/note'
+import type { NotesService } from './services/note.service'
+import type { NotificationModule } from './modules/notification/notification.module'
+
+export class NotesApplication {
+  private notesService: NotesService
+  private notifier: NotificationModule
+
+  constructor(notesService: NotesService, notifier: NotificationModule) {
+    this.notesService = notesService
+    this.notifier = notifier
+  }
+
+  private notifyError(res: AppError) {
+    this.notifier.notify('error', res.message)
+  }
+
+  async getAll(
+    params?: GetAllParams,
+  ): Promise<AppSuccess<PaginateResult<Note>> | AppError> {
+    const res = await this.notesService.getAll(params)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async create(dto: CreateNoteDTO): Promise<AppSuccess<Note> | AppError> {
+    const res = await this.notesService.create(dto)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async getOne(id: number): Promise<AppSuccess<Note> | AppError> {
+    const res = await this.notesService.getOne(id)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async update(
+    id: number,
+    dto: UpdateNoteDTO,
+  ): Promise<AppSuccess<Note> | AppError> {
+    const res = await this.notesService.update(id, dto)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async delete(id: number): Promise<AppSuccess<null> | AppError> {
+    const res = await this.notesService.delete(id)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+}
---
+++ app/application/todo.application.ts
@@ -0,0 +1,66 @@
+import { AppError } from '~/types/app-errors'
+import { AppSuccess, type GetAllParams, type PaginateResult } from '~/types/app.types'
+import type {
+  CreateTodoDTO,
+  CreateTodoResponse,
+  ReplaceTodoDTO,
+  Todo,
+  UpdateTodoDTO,
+} from '~/types/todo'
+import type { ID } from '~/types/general'
+import type { TodoService } from './services/todo.service'
+import type { NotificationModule } from './modules/notification/notification.module'
+
+export class TodoApplication {
+  private todoService: TodoService
+  private notifier: NotificationModule
+
+  constructor(todoService: TodoService, notifier: NotificationModule) {
+    this.todoService = todoService
+    this.notifier = notifier
+  }
+
+  private notifyError(res: AppError) {
+    this.notifier.notify('error', res.message)
+  }
+
+  async create(
+    dto: CreateTodoDTO,
+  ): Promise<AppSuccess<CreateTodoResponse> | AppError> {
+    const res = await this.todoService.create(dto)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async getAll(
+    params?: GetAllParams,
+  ): Promise<PaginateResult<Todo> | AppError> {
+    const res = await this.todoService.getAll(params)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async getOne(id: ID): Promise<AppSuccess<Todo> | AppError> {
+    const res = await this.todoService.getOne(id)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async replace(id: ID, dto: ReplaceTodoDTO): Promise<void | AppError> {
+    const res = await this.todoService.replace(id, dto)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async update(id: ID, dto: UpdateTodoDTO): Promise<void | AppError> {
+    const res = await this.todoService.update(id, dto)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+
+  async delete(id: ID): Promise<void | AppError> {
+    const res = await this.todoService.delete(id)
+    if (res instanceof AppError) this.notifyError(res)
+    return res
+  }
+}
